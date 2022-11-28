c419bb916627db36d16b3be4292a88493c3217c0
Byeongmok <74776691+Byeongmok@users.noreply.github.com>
2022-11-28 17:40:57 -0500

    Add files via upload

---
+++ MultiRLOnline_ver3.3/Actions/Action_parent.py
@@ -0,0 +1,3 @@
+class Action_parent():
+    def __init__(self):
+        self.action = ((0, 0), (0, 1), (1, 0), (0, 15), (15, 0))
---
+++ MultiRLOnline_ver3.3/Actions/Action_retailer.py
@@ -0,0 +1,5 @@
+class Action_retailer():
+    def __init__(self):
+        self.action = (0, 10, 20, 30, 40, 50, 60)
+        
+  
